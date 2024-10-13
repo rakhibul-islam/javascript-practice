e2f375ec64f07de77a58d32745785df3801ef8cb
rakhibul-islam <rakhibulislam4132@gmail.com>
2024-10-13 12:16:07 +0600

    add memory topic

---
+++ day-1/memory.js
@@ -0,0 +1,31 @@
+
+       /*
+1.stack(primitive)=> copy
+
+2.heap(non-primitive)=> reference (original value)
+
+*/
+//stack changes just copy value
+let city = "paris"
+
+let city2 = city
+  city2 = "london"
+console.log(city)
+console.log(city2)
+//heap changes original value
+let myObj = {
+    name: "badhon",
+    roll: 42
+}
+
+let myObj2 = myObj
+myObj2.roll = 66;
+console.log(myObj.roll)
+console.log(myObj2.roll)
+
+
+// output:
+// paris
+// london
+// 66
+// 66
